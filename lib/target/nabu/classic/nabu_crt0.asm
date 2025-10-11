1026a85a785a986494f49bdd301979d2ce088109
Alexander G. M. Smith <agmsmith@ncf.ca>
2025-10-10 22:29:05 -0400

    WIP - More Nabu Bare subtype and C runtime.
    Discovered that -D doesn't define a symbol in the .cfg file for all tools, just the C compiler.  Use -pragma-define: instead.  Also values with a dollar sign become zero.

--- lib/target/nabu/classic/nabu_crt0.asm
+++ lib/target/nabu/classic/nabu_crt0.asm
@@ -1,12 +1,12 @@
 ;
-;	Startup for Nabu
+;	Startup for Nabu, a circa 1981 Z80 based computer.
 ;
 
     module  nabu_crt0 
 
 
 ;--------
-; Include zcc_opt.def to find out some info
+; Include zcc_opt.def to access defines etc, dynamically made at build time.
 ;--------
 
     defc    crt0 = 1
@@ -17,29 +17,26 @@
 ;--------
 
     EXTERN  _main           ;main() is always external to crt0 code
-
-    PUBLIC  __Exit         ;jp'd to by exit()
-    PUBLIC  l_dcal          ;jp(hl)
-
-IF DEFINED_CRT_ORG_BSS
-    defc    __crt_org_bss = CRT_ORG_BSS
-ENDIF
+    PUBLIC  __Exit          ;jp'd to by exit()
+    PUBLIC  l_dcal          ;jp(hl) - used by compiler to jump indirect.
 
 IFNDEF      CRT_ORG_CODE
     defc    CRT_ORG_CODE = 0x0000
 ENDIF
 
-IF CRT_ORG_CODE = 0x0000
-    ; By default we don't have any rst handlers
-    defc    TAR__crt_enable_rst = $0000
-ENDIF
+    ; By default we don't have any rst handlers, since the interrupt table
+    ; doesn't start at location 0.
+    defc    TAR__crt_enable_rst = 0
 
-IFDEF __NABU_BARE__
-    ; Bare subtype, no stdio etc.  Assumes you are using DJ Sures' NABU_LIB
-    ; which is a  user level hardware library included as source code.
-    defc    TAR__fputc_cons_generic = 0
+IF __NABU_BARE__
+    ; "Bare" subtype, no stdio etc.  Assumes you are using DJ Sures' NABU_LIB
+    ; (see https://nabu.ca/) which is a hardware library included as source
+    ; code that does everything from VDP print support to interrupt handling.
+;    defc    TAR__fputc_cons_generic = 0 ; So our fputc_cons_native gets used.
+;    defc    TAR__fgetc_cons_inkey = 0
+;    defc    TAR__no_ansifont = 1
 ELSE
-    ; Default subtype has console output and input.
+    ; Subtype "Default" has console output and input.
     defc    TAR__fputc_cons_generic = 1
     defc    CONSOLE_COLUMNS = 32
 IF !DEFINED_CONSOLE_ROWS
@@ -54,6 +51,8 @@ ENDIF
     EXTERN  vdp_set_mode
 ENDIF
 
+    ; Need these for switching ROM bank in and out, control bits are in an
+    ; I/O port on the sound chip of all places.
     PUBLIC  PSG_AY_REG
     PUBLIC  PSG_AY_DATA
     defc    PSG_AY_REG = $40
@@ -73,9 +72,11 @@ ENDIF
     ; them, so put in 0 = NOP.  May have been used for a 24 bit size of the
     ; segment or something else NABU Networky.
     defb    0,0,0
-    defw    __CPU_CLOCK ; Just testing defines from nabu.cfg.
+    jr      start
+    defq    __CPU_CLOCK ; TODO: Remove.  Just testing defines from nabu.cfg.
 start:
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
+    ; Save stack pointer by modifying code, so it gets restored on exit.
     ld      (__restore_sp_onexit+1),sp
 ENDIF
     INCLUDE "crt/classic/crt_init_sp.inc"
@@ -88,14 +89,14 @@ ENDIF
     ; Setup BSS memory and perform other initialisation
     call    crt0_init
 
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
     ; Code is shared with CP/M. This is a noop, but pulls in code
     ; into crt0_init and crt0_exit
     call    cpm_platform_init 
 ENDIF
 
     INCLUDE "crt/classic/crt_init_atexit.inc"
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
     INCLUDE "crt/classic/tms99x8/tms99x8_mode_init.inc"
 ENDIF
     INCLUDE "crt/classic/crt_init_heap.inc"
@@ -108,19 +109,20 @@ __Exit:
     push    hl ; Save exit code.
     call    crt0_exit
 
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
     INCLUDE "crt/classic/tms99x8/tms99x8_mode_exit.inc"
 ENDIF
 
     pop     bc
     INCLUDE "crt/classic/crt_exit_eidi.inc"
 
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
 __restore_sp_onexit:
-    ld      sp,0  ; Modified code here with saved stack pointer.
+    ld      sp,0  ; Modified code in here with saved stack pointer.
     ret
 ELSE
-    ; Really should switch ROM bank in and jump to the reset vector.
+    ; Really should switch ROM bank in and jump to the reset vector, though we
+    ; may be running code in the RAM area used by the ROM so that may not work.
 BareEnd:
     halt
     jr BareEnd
@@ -129,11 +131,13 @@ ENDIF
 l_dcal:
     jp      (hl)
 
-
-    INCLUDE "crt/classic/crt_runtime_selection.inc" 
+IF !__NABU_BARE__
+    ; Selects print formats and stdio functions to use.
+    INCLUDE "crt/classic/crt_runtime_selection.inc"
+ENDIF
     INCLUDE	"crt/classic/crt_section.inc"
 
-IFNDEF __NABU_BARE__
+IF !__NABU_BARE__
     INCLUDE "target/nabu/classic/nabu_hccabuf.asm"
     ; And include handling disabling screenmodes
     INCLUDE "crt/classic/tms99x8/tms99x8_mode_disable.inc"
